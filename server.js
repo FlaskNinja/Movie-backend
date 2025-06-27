b0ec5a6d748968e7371f10c198dca83ccdbd2e0c
FlaskNinja <usmanmariam43@gmail.com>
2025-06-27 18:14:23 +0100

    Update server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const genreRoutes = require("./routes/genreRoutes");
 const discoverRoutes = require("./routes/discoverRoutes");
 const userRoutes = require('./routes/userRoutes');
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 
 const app = express();
